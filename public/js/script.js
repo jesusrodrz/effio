759ef8806d262802902335f66845563e3bf86d2e
Jesus Rodriguez <jesus.rodrz97@gmail.com>
2018-06-25 17:41:17 -0400

    inventario

---
+++ public/js/script.js
@@ -0,0 +1,50 @@
+
+function navBar() {
+  const navBar = document.getElementById('mainNav'),
+    navTrigger = document.getElementById('navTrigger'),
+    menu = navBar.querySelector('.menu');
+
+
+  if (navBar && navTrigger) {
+
+    let navState = false;
+    let subMenuC = null
+    if (window.innerWidth < 864) {
+
+      navBar.addEventListener('click', e => {
+        if (e.target.closest('#navTrigger')) {
+          e.preventDefault();
+          navTrigger.classList.toggle('open')
+          menu.classList.toggle('open')
+
+        }
+        if (e.target.closest('.has-sub-menu.menu__link')) {
+          const subMenu = e.target.parentElement.querySelector('.menu--sub-menu');
+          // console.log(subMenu)
+          e.target.classList.toggle('open');
+          subMenu.classList.toggle('open');
+
+        }
+        if (e.target.closest('.has-sub-menu.menu__link--sub-menu')) {
+          const subMenu = e.target.parentElement.querySelector('.menu--sub-menu-l2');
+          // console.log(subMenu, subMenuC)
+          e.target.classList.toggle('open');
+          subMenu.classList.toggle('open');
+          if (subMenuC && subMenuC !== subMenu) {
+
+            subMenuC.classList.toggle('open')
+            subMenuC = subMenu;
+          } else if (!subMenuC) {
+
+            subMenuC = subMenu;
+          }
+        }
+      })
+    }
+  }
+}
+navBar()
+
+window.addEventListener('click', e => {
+  console.log(e.target)
+})
